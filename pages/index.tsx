d99954306e8e87aceccb0b7c8f7fb783af300621
telmogoncalves <telmogoncalves@me.com>
2021-02-10 20:52:09 +0000

    ESLint + Prettier Article

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-function Homepage() {
+function Homepage(): JSX.Element {
   return (
-    <h1 className="text-center my-24 font-black tracking-tight text-6xl">Our homepage</h1>
+    <h1 className="text-center my-24 font-black tracking-tight text-6xl">
+      Our homepage
+    </h1>
   )
 }
 
 export default Homepage
-
